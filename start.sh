0f2c2ebe36aba4293865e91ce3fd60b66d5c531b
Brian Peiris <brianpeiris@gmail.com>
2019-07-10 23:49:23 -0700

    fix start script and configs

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
-npx tsc *.ts -m ESNext -w
-npx rollup index.js --format iife --file bundle.js -c -w
-browser-sync -c .browser-sync.config.js
+echo running tsc
+npx tsc src/*.ts -m ESNext -w &
+
+echo running rollup
+npx rollup src/index.js --format iife --file bundle.js -c -w &
+
+echo running bs
+browser-sync -c .browser-sync.config.js &
+
+trap 'trap - SIGTERM && kill 0' SIGTERM EXIT
+wait
